5c764c299c5439a8b89ffee4c0460b1278693cbc
syeminpark <70131115+syeminpark@users.noreply.github.com>
2020-11-15 20:09:43 +0900

    sandcastle
    최종 목표는 바람에 흩날리는 모래성 느낌을 만들어내보려 한다. 우선은 이미지 대신 ellipse을 사용하도록 변경하여 좀 더 가볍게 만들었다. 그리고 색깔도 모래 색에 맞게 변경하였다.

--- NOC_4_08_ParticleSystemSmoke/particle.js
+++ NOC_4_08_ParticleSystemSmoke/particle.js
@@ -17,12 +17,14 @@ class Particle {
   run() {
     this.update();
     this.render();
+
   }
 
   // Method to apply a force vector to the Particle object
   // Note we are ignoring "mass" here
   applyForce(f) {
     this.acc.add(f);
+
   }
 
   // Method to update position
@@ -33,15 +35,11 @@ class Particle {
     this.acc.mult(0); // clear Acceleration
   }
 
-  // Method to display
   render() {
-    imageMode(CENTER);
-    tint(255, this.lifespan);
-    image(img, this.pos.x, this.pos.y);
-    // Drawing a circle instead
-    // fill(255,lifespan);
-    // noStroke();
-    // ellipse(pos.x,pos.y,img.width,img.height);
+
+    fill(255,250,200,this.lifespan);
+    noStroke();
+    ellipse(this.pos.x,this.pos.y,img.width,img.height);
   }
 
   // Is the particle still useful?
